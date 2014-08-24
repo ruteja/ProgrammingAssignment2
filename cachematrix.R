512677336c65a7d78d2cc554d4b94dbca20d8c42
Ruteja <ruteja@gmail.com>
2014-08-24 12:35:05 -0500

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -5,14 +5,14 @@
 ## namely set,get, setsolve and getsolve to access the matrix and the inverse values.
 
 makeCacheMatrix <- function(x = numeric()) {
-  s <- NULL ## inital value of inverse is null.
-  set <- function(y) ## takes input
+  m <- NULL ## inital value of inverse is null.
+  set <- function(y) {## takes input
     x <<- y ## saves  to x
-    s <<- NULL ## sets inverse to null
+    m <<- NULL ## sets inverse to null
   }
   get <- function() x ## function returns original value of x
-  setsolve <- function(solve) s <<- solve ## superassigns inverse (solve) to s
-  getsolve <- function() s ##  Called from cacheSolve if inverse is not already calculated
+  setsolve <- function(solve) m <<- solve ## superassigns inverse (solve) to m
+  getsolve <- function() m ##  Called from cacheSolve if inverse is not already calculated
   list(set = set, get = get,
        setsolve = setsolve,
        getsolve = getsolve) ## function can return only one value so a list is returned
@@ -21,13 +21,13 @@ makeCacheMatrix <- function(x = numeric()) {
 ## function CacheSolve gives the inverse of the matrix created in makeCacheMatrix.
 # if the inverse is already calculated, uses the cached value. If not calls the solve function and return the inverse value
 cacheSolve <- function(x, ...) { ## takes matrix created by makeCacheMatrix
-  s <- x$getsolve() ## access inverse obtained from makeCacheMAtrix
-  if(!is.null(s)) { ## if inverse is not null, ie already calculated returns cached value
+  m <- x$getsolve() ## access inverse obtained from makeCacheMAtrix
+  if(!is.null(m)) { ## if inverse is not null, ie already calculated returns cached value
     message("getting cached data") ## prints message that cached data used
-    return(s) ## returns the solution
+    return(m) ## returns the solution
   }
   data <- x$get() ## if inverse is null, gets the data by using the get subfunction
-  s <- solve(data, ...) ## calculates inverse
-  x$setsolve(s) ## sets the inverse to s
-  s ## returns inverse
+  m <- solve(data, ...) ## calculates inverse
+  x$setsolve(m) ## sets the inverse to m 
+  m ## returns inverse
 }
